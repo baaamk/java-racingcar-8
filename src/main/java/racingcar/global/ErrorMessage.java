3dde89b2c4d70428176d1fd7094194fd32c3fafe
baaamk <brandon55321@gmail.com>
2025-10-25 01:09:45 +0900

    feat(global): 에러메세지 상수 관리 구현

---
+++ src/main/java/racingcar/global/ErrorMessage.java
@@ -0,0 +1,10 @@
+package racingcar.global;
+
+public class ErrorMessage {
+    public static final String INVALID_CAR_NAME_NULL = "이름이 NULL이여서는 안됩니다.";
+    public static final String INVALID_CAR_NAME_EMPTY = "이름이 입력되지 않았습니다.(1자 미만 입력)";
+    public static final String INVALID_CAR_NAME_MAX_LENGTH = "문자 길이는 1자 이상 5자 이하여야 합니다.(5자 초과 입력)";
+    public static final String INVALID_CAR_NAME_WHITESPACE = "이름 사이에 공백이 있어서는 안됩니다.";
+    public static final String INVALID_CAR_NAME_BLANK = "이름이 비어있으면 안됩니다.";
+
+}
