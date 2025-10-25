9c2651b68379241a75a761d5d38dd2a896ff4d46
baaamk <brandon55321@gmail.com>
2025-10-25 20:01:28 +0900

    feat(domain): RacingCars 일급 컬렉션 및 자동차 이동/깊은 복제 로직 구현

---
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -0,0 +1,44 @@
+    package racingcar.domain;
+
+    import camp.nextstep.edu.missionutils.Randoms;
+    import racingcar.global.ErrorMessage;
+
+    import java.util.List;
+
+    public class RacingCars{
+        private final List<RacingCar> racingCars;
+
+        private RacingCars(List<RacingCar> racingCars) {
+            validate(racingCars);
+            this.racingCars = List.copyOf(racingCars);
+        }
+
+        public static RacingCars from(List<RacingCar> racingCars) {
+            return new RacingCars(racingCars);
+        }
+
+        public void play() {
+            for (RacingCar racingCar : racingCars) {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+                racingCar.move(randomNumber);
+            }
+        }
+
+        public RacingCars copyRacingCars() {
+            List<RacingCar> clonedRacingCars = racingCars.stream()
+                    .map(RacingCar::clone)
+                    .toList();
+            return RacingCars.from(clonedRacingCars);
+        }
+
+        private static void validate(List<RacingCar> racingCars) {
+            if (racingCars == null || racingCars.isEmpty()) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_NULL);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return racingCars.toString();
+        }
+    }
