423906090ba8d92d771b13b0e9cc86208f03c05a
baaamk <brandon55321@gmail.com>
2025-10-26 05:03:04 +0900

    feat(view): 출력 view 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,11 @@
 package racingcar.view;
 
+import racingcar.controller.dto.RoundResultDto;
+import racingcar.controller.dto.WinnerDto;
+
 public interface OutputView {
     public void initialPrint();
     public void NumberRequestPrint();
-    public void roundResultPrint();
-    public void winnerPrint();
+    public void roundResultPrint(RoundResultDto roundResultDto);
+    public void winnerPrint(WinnerDto winnerDto);
 }
--- src/main/java/racingcar/view/OutputViewImpl.java
+++ src/main/java/racingcar/view/OutputViewImpl.java
@@ -1,5 +1,13 @@
 package racingcar.view;
 
+import racingcar.controller.dto.RoundResultDto;
+import racingcar.controller.dto.WinnerDto;
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingCars;
+
+import java.util.List;
+import java.util.Map;
+
 public class OutputViewImpl implements OutputView {
 
     @Override
@@ -13,12 +21,35 @@ public class OutputViewImpl implements OutputView {
     }
 
     @Override
-    public void roundResultPrint() {
-        System.out.println("실행 결과");
+    public void roundResultPrint(RoundResultDto roundResultDto) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("실행 결과").append("\n");
+
+        roundResultDto.getRacingResults().entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .forEach(entry -> {
+                    RacingCars racingCars = entry.getValue();
+
+                    racingCars.getRacingCars().forEach(car -> {
+                        sb.append(car.getCarName())
+                                .append(" : ")
+                                .append("-".repeat(car.getState()))
+                                .append("\n");
+                    });
+
+                    sb.append("\n");
+                });
+        System.out.println(sb);
+
     }
 
     @Override
-    public void winnerPrint() {
-        System.out.print("최종 우승자 : ");
+    public void winnerPrint(WinnerDto winnerDto) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("최종 우승자 : ");
+        List<String> list = winnerDto.getRacingCars().stream().map(RacingCar::getCarName).toList();
+        sb.append(String.join(", ", list));
+
+        System.out.println(sb);
     }
 }
