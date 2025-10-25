9767970f3be39ca3605e1e281a087df8947514d3
baaamk <brandon55321@gmail.com>
2025-10-25 23:05:59 +0900

    test: 예외 테스트 구현

--- src/test/java/racingcar/infrastructure/ParserTest.java
+++ src/test/java/racingcar/infrastructure/ParserTest.java
@@ -2,8 +2,11 @@ package racingcar.infrastructure;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.global.ErrorMessage;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ParserTest {
 
@@ -29,6 +32,34 @@ class ParserTest {
     void 숫자_입력_공백_제거_테스트() {
         int racingCars= Parser.parseToTryNumber(" 5 ");
         Assertions.assertThat(racingCars).isEqualTo(5);
+    }
+
+    @Test
+    void 이름_구분자_동일_예외_테스트() {
+        assertThatThrownBy(() -> Parser.parseToRacingCars(",pobi,,,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_CAR_NAME_AS_DELIMITER);
+    }
+
+    @Test
+    void 빈_이름_예외_테스트() {
+        assertThatThrownBy(() -> Parser.parseToRacingCars(",pobi,,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_CAR_NAME_EMPTY);
+    }
+
+    @Test
+    void 잘못된_구분자_예외_테스트() {
+        assertThatThrownBy(() -> Parser.parseToRacingCars("pobi.java.suji"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_CAR_NAME_DELIMITER);
+    }
+
+    @Test
+    void 빈_이름_예외_테스트2() {
+        assertThatThrownBy(() -> Parser.parseToRacingCars(",,,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_CAR_NAME_AS_DELIMITER);
     }
 
 }
