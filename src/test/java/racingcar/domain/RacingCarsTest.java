31b29cc2171a865bc600e81231cdf360174439fd
baaamk <brandon55321@gmail.com>
2025-10-25 20:03:12 +0900

    test: 상태변화 및 예외 테스트 구현

---
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+import racingcar.global.ErrorMessage;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class RacingCarsTest {
+
+    @Test
+    void play_호출시_자동차중_일부는_상태가_변할_수_있다() {
+        RacingCar pobi = RacingCar.from("pobi");
+        RacingCar java = RacingCar.from("java");
+        RacingCars racingCars = RacingCars.from(List.of(pobi, java));
+
+        racingCars.play();
+
+        assertThat(pobi.getState()).isGreaterThanOrEqualTo(0);
+        assertThat(java.getState()).isGreaterThanOrEqualTo(0);
+
+    }
+
+    @Test
+    void 비어있는_자동차리스트_생성_예외() {
+        assertThatThrownBy(() -> RacingCars.from(List.of()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_CAR_NAME_NULL);
+    }
+
+}
