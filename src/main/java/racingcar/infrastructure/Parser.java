d30de250445109ba32a17bbd9a3fa3584d20814d
baaamk <brandon55321@gmail.com>
2025-10-25 23:05:33 +0900

    feat(infrastructure): 이름 입력 검증 기능 구현

--- src/main/java/racingcar/infrastructure/Parser.java
+++ src/main/java/racingcar/infrastructure/Parser.java
@@ -1,5 +1,7 @@
 package racingcar.infrastructure;
 
+import racingcar.global.ErrorMessage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +9,7 @@ public class Parser {
     private Parser() {}
 
     public static List<String> parseToRacingCars(String inputNames) {
+        validateDelimiterUsage(inputNames);
         return Arrays.stream(inputNames.split(","))
                 .map(String::trim)
                 .toList();
@@ -16,4 +19,17 @@ public class Parser {
         String trimmedNumber = inputTryNumber.trim();
         return Integer.parseInt(trimmedNumber);
     }
+
+    private static void validateDelimiterUsage(String inputNames) {
+        if (inputNames.contains(",,,")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_AS_DELIMITER);
+        }
+        if (inputNames.contains(",,") || inputNames.startsWith(",") || inputNames.endsWith(",")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_EMPTY);
+        }
+        if (!inputNames.contains(",")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_DELIMITER);
+        }
+    }
+
 }
