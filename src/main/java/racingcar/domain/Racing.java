9abab131ad5b1e487d39c575dfa17e9f5a345e37
baaamk <brandon55321@gmail.com>
2025-10-27 23:33:03 +0900

    refactor(domain): 자동차 전진 저장되도록 수정

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -4,27 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Racing {
-    private final int totalRound;
+    private final TryNumber totalRound;
     private final RacingCars racingCars;
-    private final Map<Integer, RacingCars> roundResult;
+    private final Map<Integer, RacingCars> roundResults;
 
-    private Racing(int totalRound, RacingCars racingCars, Map<Integer, RacingCars> roundResult) {
+    private Racing(TryNumber totalRound, RacingCars racingCars, Map<Integer, RacingCars> roundResults) {
         this.totalRound = totalRound;
         this.racingCars = racingCars;
-        this.roundResult = roundResult;
+        this.roundResults = roundResults;
     }
 
-    public static Racing of(int totalRound, RacingCars racingCars) {
+    public static Racing of(TryNumber totalRound, RacingCars racingCars) {
         Map<Integer, RacingCars> roundResult = new HashMap<>();
         return new Racing(totalRound, racingCars, roundResult);
     }
 
     public Map<Integer, RacingCars> playRacing() {
-        for (int i = 1; i <= totalRound; i++) {
-            racingCars.play();
-            roundResult.put(i, racingCars.copyRacingCars());
+        RacingCars currentCars = racingCars;
+        for (int i = 1; i <= totalRound.getTryNumber(); i++) {
+            currentCars = currentCars.play();
+            roundResults.put(i, currentCars);
         }
-        return roundResult;
+        return roundResults;
     }
 
 }
